d83ff70cf909ab0a4ba079bb649f2d423c5085c7
Lucas Netto <159018669+LucasNetto1@users.noreply.github.com>
2024-05-15 08:18:11 -0300

    Add files via upload

---
+++ pagina de ligado e desligado/ligadoedesligado.js
@@ -0,0 +1,9 @@
+
+function ligado() {
+window.location.href = "Aumento de Temperatura.html"
+}
+
+function desligado() {
+document.write("<font size='7'> AR-CONDICIONADO DESLIGADO COM SUCESSO</font>")
+document.write("<a href='ligadoedesligado.html'>Clique aqui para voltar à página de ligamento do ar-condicionado</a>")
+}
